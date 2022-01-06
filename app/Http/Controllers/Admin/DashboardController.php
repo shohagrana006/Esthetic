68ec8fbaa0867ed6b2e0c5bb814d8f550d2729d3
Firozmahamud <firozmahmud593@gmail.com>
2022-01-06 17:22:35 +0600

    dashboard filter added 2

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -18,54 +18,41 @@ class DashboardController extends Controller
 
     public function index(){
 
-        // $today_date = date('Y-m-d');
-        // $month_date = date('m');
-        $year_date = date('Y');
         //expence
-        // $previous_month_expenses = Expense::whereMonth('created_at', date('m', strtotime('-1 month')))->get();
-
-        $year_expenses = Expense::whereYear('created_at', $year_date)->get();
-        $previous_year_expenses = Expense::whereYear('created_at', date('Y', strtotime('-1 year')))->get();
-
         $expenses = Expense::all();
 
         //purchage
-
-        // $previous_month_purchages = Purchage::whereMonth('created_at', date('m', strtotime('-1 month')))->get();
-
-        $year_purchages = Purchage::whereYear('created_at', $year_date)->get();
-        $previous_year_purchages = Purchage::whereYear('created_at', date('Y', strtotime('-1 year')))->get();
-
         $purchages = Purchage::all();
 
         // Sell Value
-        // $previous_month_product = Product::whereMonth('created_at', date('m', strtotime('-1 month')))->get();
-
-        $year_product = Product::whereYear('created_at', $year_date)->get();
-        $previous_year_product = Product::whereYear('created_at', date('Y', strtotime('-1 year')))->get();
-
-
-
         $product = Product::all();
 
         // Damage
-        // $previous_month_damage = Damage::whereMonth('created_at', date('m', strtotime('-1 month')))->get();
-
-        $year_damage = Damage::whereYear('created_at', $year_date)->get();
-        $previous_year_damage = Damage::whereYear('created_at', date('Y', strtotime('-1 year')))->get();
-
-
-
 
         $damage = Damage::all();
 
 
 
-        return view('admin.home',compact(  'year_expenses', 'previous_year_expenses', 'expenses','purchages','product','damage',
-    'year_purchages','previous_year_purchages','year_product','previous_year_product','year_damage','previous_year_damage'
-    ));
+        return view('admin.home',compact(  'expenses','purchages','product','damage'));
     }
 
+
+    public function previous_year($previous_year_date = null)
+    {
+        if ($previous_year_date == null) {
+            $previous_year_date = date('Y');
+        }
+
+
+        $previous_year_expenses = Expense::whereYear('created_at', date('Y', strtotime('-1 year')))->get();
+        $previous_year_purchages = Purchage::whereYear('created_at', date('Y', strtotime('-1 year')))->get();
+        $previous_year_product = Product::whereYear('created_at', date('Y', strtotime('-1 year')))->get();
+        $previous_year_damage = Damage::whereYear('created_at', date('Y', strtotime('-1 year')))->get();
+
+        return view('admin.filter.previousyear', compact('previous_year_date','previous_year_expenses','previous_year_purchages','previous_year_product','previous_year_damage'));
+
+
+    }
 
     public function today()
     {
@@ -126,9 +113,19 @@ class DashboardController extends Controller
 
     }
 
+    public function yearly($year_date = null)
+    {
+        if ($year_date == null) {
+            $year_date = date('Y');
+        }
+        $year_expenses = Expense::whereYear('created_at', $year_date)->get();
+        $year_purchages = Purchage::whereYear('created_at', $year_date)->get();
+        $year_product = Product::whereYear('created_at', $year_date)->get();
+        $year_damage = Damage::whereYear('created_at', $year_date)->get();
+        return view('admin.filter.year', compact('year_date','year_expenses','year_purchages','year_product','year_damage'));
 
 
-
+    }
 
 
 }
